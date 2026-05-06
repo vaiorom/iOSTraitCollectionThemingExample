790d9dc496e2ebc1fbf5f15af9239fab4fc54b20
vaiorom <aucggl@gmail.com>
2026-05-06 17:21:59 +0900

    Remove ViewController class and add print statement
    Removed the ViewController class and added a print statement.

--- ThemingExample/ViewController.swift
+++ ThemingExample/ViewController.swift
@@ -1,28 +1,3 @@
-//
-//  ViewController.swift
-//  ThemingExample
-//
-//  Created by Dan Loewenherz on 1/20/25.
-//
+import Foundation
 
-import UIKit
-
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = Theme.backgroundColor
-  }
-
-  @IBAction
-  func toggleTheme() {
-    let oldTheme = ThemeStore.shared.getTheme()
-    let newTheme = oldTheme == .dark ? Theme.light : Theme.dark
-
-    guard let windowScene = view.window?.windowScene else { return }
-
-    // Update the current theme in the theme store and update the trait overrides.
-    ThemeStore.shared.setTheme(newTheme)
-    windowScene.traitOverrides.theme = newTheme
-  }
-}
+print("Hello, iOS build from Windows!")
